6255bd2c83e1479d7d17ea75de2bf26c728be0db
Keith <shinugayoi@gmail.com>
2014-05-28 03:19:56 -0500

    Updated to include possibility of docstrings and stuff

--- completions.py
+++ completions.py
@@ -1,7 +1,7 @@
 import sublime, sublime_plugin, string, re
 
 
-# TODO: Add defined functions to completions
+# TODO: Add defined functions to dictionary
 
 
 class Library():
@@ -12,10 +12,9 @@ class Library():
 		# object containing completions filenames
 		self.actives = self.settings.get("completions_file_list")
 
-		# currently only dnh
 		if self.actives:
 			for key in self.actives:
-				# load file contents into dictionary
+				# load file contents into dictionary with scope as key
 				# make sure it's a settings file
 				self.dict[key] = sublime.load_settings(self.actives[key])
 
@@ -33,29 +32,36 @@ else:
 class LibraryCollector(sublime_plugin.EventListener):
 	global library
 	def on_query_completions(self, view, prefix, locations):
-		self.completions = []
+		self.dict = []
 
 		for key in library.dict:
-			if (library.actives and library.actives.get(key)): # if dict active
+			# if dict active
+			if (library.actives and library.actives.get(key)): 
 				scope = library.dict[key].get("scope")
+				# if dict matches current scope
 				if scope and view.match_selector(locations[0], scope):
-					self.completions += library.dict[key].get("completions")
+					self.dict += library.dict[key].get("dict")
 
-		if not self.completions:
+		if not self.dict:
 			return []
 
-		# extend other completions
-		olds = [view.extract_completions(prefix)]
-		olds = [(item, item) for sublist in olds for item in sublist if len(item) > 3]
-		olds = list(set(olds))
-		self.completions = clean(self.completions, olds)
-		return (self.completions)
+		# extend existing completions list
+		oldcomp = [view.extract_completions(prefix)]
+		oldcomp = [(item, item) for sublist in oldcomp for item in sublist if len(item) > 3]
+		oldcomp = list(set(oldcomp))
+		newcomp = extend_comp(self.dict, oldcomp)
+		return (newcomp)
 
-def clean(completions, olds):
+def extend_comp(dict, oldcomp):
 	newcomp = []
-	for string in list(completions):
+	for obj in list(dict):
 		# validate and split
-		match = re.findall(r"^(\w+::)?(\w+)\((.*)\)(\t.*)?$", string)[0]
+		matchlist = re.findall(r"^(\w+::)?(\w+)\((.*)\)(\t.*)?$", obj["sig"])
+
+		# if invalid string is found
+		if (len(matchlist) == 0): continue
+
+		match = matchlist[0]
 		# [0]: namespace
 		# [1]: function name
 		# [2]: parameters
@@ -63,28 +69,30 @@ def clean(completions, olds):
 
 		name = match[1]
 		# remove duplicates
-		if (name, name) in olds:
-			olds.remove((name, name))
+		if (name, name) in oldcomp:
+			oldcomp.remove((name, name))
 
 		# generate tab-to-params string
 		params = "(" + ", ".join(["$"+str(i) for i in list(range(1, match[2].count(",")+2))]) + ")$0"
 
 		# add to completions list
-		newcomp.append(tuple([string] + [name + params]))
+		newcomp.append(tuple([obj["sig"]] + [name + params]))
 
-	newcomp.extend(olds)
+	newcomp.extend(oldcomp)
 	return newcomp
 
-
+# TODO: Hook up with dict
+# TODO: Need to generalize
+# TODO: Multiple lines
 class DanmakufuDoc(sublime_plugin.TextCommand):
 	def run(self, edit):
-		if(not self.view.match_selector(self.view.sel()[0].begin(), 'source.danmakufu')): return
+		if(not self.view.match_selector(self.view.sel()[0].begin(), "source.danmakufu")): return
 
-		panel = self.view.window().get_output_panel('danmakufu_doc')
-		self.view.window().run_command('show_panel', {'panel': 'output.danmakufu_doc'})
+		panel = self.view.window().get_output_panel("danmakufu_doc")
+		self.view.window().run_command("show_panel", {"panel": "output.danmakufu_doc"})
 
 		panel.set_read_only(False)
-		panel.run_command('insert', {'characters': 'aaa'})
+		panel.run_command("insert", {"characters": "aaa"})
 		panel.set_read_only(True)
 
 		# hide after timeout
@@ -92,5 +100,5 @@ class DanmakufuDoc(sublime_plugin.TextCommand):
 
 		def hide(panel):
 			if(bool(panel.window())):
-				panel.window().run_command('hide_panel')
+				panel.window().run_command("hide_panel")
 
