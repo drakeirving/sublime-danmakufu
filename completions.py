cc013d9e1c51f0dd235b628df1880e806a6b29e1
Keith <shinugayoi@gmail.com>
2014-05-29 02:45:42 -0500

    implemented function documentation capability

--- completions.py
+++ completions.py
@@ -29,7 +29,7 @@ else:
 		library.init()
 
 
-class LibraryCollector(sublime_plugin.EventListener):
+class DictCollector(sublime_plugin.EventListener):
 	global library
 	def on_query_completions(self, view, prefix, locations):
 		self.dict = []
@@ -50,6 +50,7 @@ class LibraryCollector(sublime_plugin.EventListener):
 		oldcomp = [(item, item) for sublist in oldcomp for item in sublist if len(item) > 3]
 		oldcomp = list(set(oldcomp))
 		newcomp = extend_comp(self.dict, oldcomp)
+		
 		return (newcomp)
 
 def extend_comp(dict, oldcomp):
@@ -68,6 +69,7 @@ def extend_comp(dict, oldcomp):
 		# [3]: return type
 
 		name = match[1]
+
 		# remove duplicates
 		if (name, name) in oldcomp:
 			oldcomp.remove((name, name))
@@ -79,26 +81,66 @@ def extend_comp(dict, oldcomp):
 		newcomp.append(tuple([obj["sig"]] + [name + params]))
 
 	newcomp.extend(oldcomp)
-	return newcomp
+	return (newcomp)
 
-# TODO: Hook up with dict
-# TODO: Need to generalize
-# TODO: Multiple lines
-class DanmakufuDoc(sublime_plugin.TextCommand):
+
+class GetFunctionDocs(sublime_plugin.TextCommand):
+	global library
 	def run(self, edit):
-		if(not self.view.match_selector(self.view.sel()[0].begin(), "source.danmakufu")): return
+		self.dict = []
+		location = self.view.sel()[0].begin()
 
-		panel = self.view.window().get_output_panel("danmakufu_doc")
-		self.view.window().run_command("show_panel", {"panel": "output.danmakufu_doc"})
+		for key in library.dict:
+			scope = library.dict[key].get("scope")
+			# if dict matches current scope
+			if scope and self.view.match_selector(location, scope):
+				self.dict = library.dict[key].get("dict")
+				break
+
+		if not self.dict:
+			return
+
+		word = self.view.substr(self.view.word(location))
+		doc = ""
+
+		for obj in list(self.dict):
+			matchlist = re.findall(r"^(\w+::)?(\w+)\((.*)\)(\t.*)?$", obj["sig"])
+
+			# if invalid string is found
+			if (len(matchlist) == 0): continue
+
+			match = matchlist[0]
+			# [0]: namespace
+			# [1]: function name
+			# [2]: parameters
+			# [3]: return type
+
+			# if function name matches
+			if word == match[1]:
+				if not obj["doc"]: 
+					obj["doc"] = "(No documentation)"
+				doc = match[0] + match[1] + "()\n" + obj["doc"]
+				break
+
+		if not doc:
+			return
+
+		panel = self.view.window().get_output_panel("get_function_docs_panel")
+		self.view.window().run_command("show_panel", {"panel": "output.get_function_docs_panel"})
 
 		panel.set_read_only(False)
-		panel.run_command("insert", {"characters": "aaa"})
+		panel.settings().set("auto_indent", False)
+		panel.run_command("insert", {"characters": doc})
 		panel.set_read_only(True)
 
-		# hide after timeout
-		sublime.set_timeout_async(lambda: hide(panel), 5000)
+		# identify current docs panel by the function it describes
+		panel.set_name(word)
 
-		def hide(panel):
-			if(bool(panel.window())):
+		# hide after timeout
+		sublime.set_timeout_async(lambda: hide(panel, word), 8000)
+
+		def hide(panel, word):
+			# if docs panel is the same one as when it was opened
+			if panel.window() and panel.name() == word:
 				panel.window().run_command("hide_panel")
 
